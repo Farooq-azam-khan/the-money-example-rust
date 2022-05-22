e846b528bccd840cf999e7646565f3b95ca1a9df
Farooq <farooq1.khan@ryerson.ca>
2022-05-22 07:29:03 -0400

    First failing test passed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+/* 
+ 1. Add amounts in two different currencies and convert the result to a set of exchange rates
+ 2. Need to be able to multiply an amount (price per share) by a number (number of shares) and receive an amount
+*/
+
+struct Dollar {
+    amount: i32
+}
+
+impl Dollar {
+    fn times(&mut self, multiplier: i32) {
+        self.amount = 10;
+    }
+}
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[test]
+fn test_multiplication() {
+    let mut five = Dollar {amount: 5};
+    five.times(2);
+    assert_eq!(10, five.amount)
 }
